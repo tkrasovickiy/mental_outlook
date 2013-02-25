b3db28f9d27719b88278dcddc274173884ba9e2f
user <user@test.ru>
2013-02-25 18:15:47 +0400

    create my project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MentalOutlook::Application.config.secret_token = 'b5d0c14c0736a69d6d7aa1d617efd556f9764632b0e88b4c5520554faad03afc2dbecc627597ae8a304e4444d5cb23f21a988d9c7f20aac5ac888a75c5a641d0'
